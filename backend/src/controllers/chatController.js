1be899e32629e755071adca582aafc76322f9801
Laila Mohamed Fikry <laila.mohamed.fikry@gmail.com>
2026-05-18 10:59:36 +0300

    feat: implement chat request controller and create message interface page

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -85,7 +85,7 @@ export const getChatRequests = async (req, res) => {
         const user_id = req.user.id;
         // Fetch incoming pending requests
         const sql = `
-            SELECT cr.*, u.first_name, u.last_name, u.profile_pic_url, u.avatar_url, p.name as pet_name 
+            SELECT cr.*, u.first_name, u.last_name, u.profile_pic_url, p.name as pet_name 
             FROM chat_requests cr
             JOIN users u ON cr.sender_id = u.id
             LEFT JOIN pets p ON cr.pet_id = p.id
