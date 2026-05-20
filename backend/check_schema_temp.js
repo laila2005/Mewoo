e5f7feffc00c1ba35c2da48875d162c4f3f6e125
Laila Mohamed Fikry <laila.mohamed.fikry@gmail.com>
2026-05-20 18:58:28 +0300

    chore: Clean up and organize root and backend directories

--- backend/check_schema_temp.js
+++
@@ -1,3 +0,0 @@
-import { query } from './src/config/db.js';
-query('SELECT column_name, data_type FROM information_schema.columns WHERE table_name = $1', ['marketplace_products'])
-.then(res => { console.log(res.rows); process.exit(0); });
